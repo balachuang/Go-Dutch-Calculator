20f418e31e20e6e290a0ec264dd39aaa562f1ed1
balachuang <balachuang@gmail.com>
2016-09-23 18:34:58 -0500

    Add party type selector

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-var partyType = [];
-
 $(document).ready(function(){
 	$(document).on('click', '.glyphicon-plus', addNewMember);
 	$(document).on('click', '.glyphicon-remove', removeMember);
-	$(document).on('change', 'input', goCalculate);
+	$(document).on('keyup', 'input', goCalculate);
+	loadPartyType();
 });
+
+function loadPartyType()
+{
+    $(partyType).each(function(index, value){
+        $('<li><a href="#" onclick="setParty(' + index + ')">' + value.typeName + '</a></li>').appendTo('#party-type-menu');
+    });
+}
 
 function addNewMember()
 {
@@ -35,6 +41,9 @@ function goCalculate()
     var total = eval($('#total').val());
     var donate = eval($('#donate').val());
     var members = $('tr.row-data');
+
+    if (total == undefined) total = 0;
+    if (donate == undefined) donate = 0;
 
     if (total == 0) return;
     if (members.length == 0) return;
@@ -70,4 +79,23 @@ function goCalculate()
         var lastPay = eval(members.last().find('input.cal-result').val());
         members.last().find('input.cal-result').val(lastPay + diff);
     }
+}
+
+function setParty(idx)
+{
+    if ($('tr.row-data').length > 1) $('tr.row-data:gt(0)').remove();
+    for (var n=0; n<partyType[idx].members.length-1; ++n)
+    {
+        var firstRaw = $('tr.row-data:eq(0)');
+        firstRaw.clone().insertAfter(firstRaw);
+    }
+    reindex();
+    for (var n=0; n<partyType[idx].members.length; ++n)
+    {
+        var thisRaw = $('tr.row-data').eq(n);
+        thisRaw.find('td:eq(0) input').val(partyType[idx].members[n].name);
+        thisRaw.find('td:eq(1) input').val(partyType[idx].members[n].donate);
+        thisRaw.find('td:eq(2) input').val(partyType[idx].members[n].base);
+    }
+    goCalculate();
 }
---
+++ partyType.js
@@ -0,0 +1,106 @@
+var partyType =
+[
+    {
+        'typeName' : '聚餐 (朋友, 同學, 同事, ...)',
+        'members' :
+        [
+            {
+                'name' : '聚餐成員 1',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '聚餐成員 2',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '聚餐成員 3',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '聚餐成員 4',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '聚餐成員 5',
+                'donate' : 0,
+                'base' : 1
+            }
+        ]
+    },
+    {
+        'typeName' : '請客 (同事升官, 長官約聚, 尾牙, 迎新送舊, ...)',
+        'members' :
+        [
+            {
+                'name' : '苦主',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '吃客 1',
+                'donate' : 0,
+                'base' : 0
+            },
+            {
+                'name' : '吃客 2',
+                'donate' : 0,
+                'base' : 0
+            },
+            {
+                'name' : '吃客 3',
+                'donate' : 0,
+                'base' : 0
+            },
+            {
+                'name' : '吃客 4',
+                'donate' : 0,
+                'base' : 0
+            },
+            {
+                'name' : '吃客 5',
+                'donate' : 0,
+                'base' : 0
+            }
+        ]
+    },
+    {
+        'typeName' : '回請 (謝師宴, 歡送同事, ...)',
+        'members' :
+        [
+            {
+                'name' : '吃客',
+                'donate' : 0,
+                'base' : 0
+            },
+            {
+                'name' : '苦主 1',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '苦主 2',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '苦主 3',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '苦主 4',
+                'donate' : 0,
+                'base' : 1
+            },
+            {
+                'name' : '苦主 5',
+                'donate' : 0,
+                'base' : 1
+            }
+        ]
+    }
+];
